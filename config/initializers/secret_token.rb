e5b524e1e839282433fed9228837f809a0748fe4
David Chua <david.cys@live.com>
2015-02-07 12:29:56 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kapi::Application.config.secret_key_base = '7f1410cd090c0da847c22f04744e711ebb7aa0fb8dbf7fd7ef1a31f41a58119cea632789afcad7ce9e3614e537583dc752deb62429b1cb1ee560ecd73cfdaf37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kapi::Application.config.session_store :cookie_store, key: '_kapi_session'
